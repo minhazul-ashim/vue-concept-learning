795f8d500dd4fd8f4942ceb31922df375388ab40
Minhazul Ashim <ashim.ashim7470@gmail.com>
2023-10-18 00:03:55 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello {{name}}</h1>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'App',
+    data() {
+        return {
+            name: 'Ashim'
+        }
+    }
+}
+</script>
+
+<style scoped></style>
